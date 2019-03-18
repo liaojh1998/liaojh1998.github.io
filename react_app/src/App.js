07ace592284cf7169963656e19cbd9e0a1eb229b
Jie Hao Liao <liaojh1998@gmail.com>
2019-03-18 02:41:10 -0500

    Using React tutorial to understand props and components

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Game from './tutorial/tictactoe.js';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Game />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
