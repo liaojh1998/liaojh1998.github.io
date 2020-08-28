30d967f11e573dd5e92851e885c2a6198d8988cd
Jie Hao Liao <liaojh1998@gmail.com>
2020-08-28 04:32:25 -0500

    Initial draft of npm run build

---
+++ precache-manifest.7a4b11ffcc959988f924addc1e927c42.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df020c18db464f1f8dd2c434f2369f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "23ecc46796554f282a70",
+    "url": "./static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "38ee5329b25f1607b7d0",
+    "url": "./static/css/main.580eb8ee.chunk.css"
+  },
+  {
+    "revision": "23ecc46796554f282a70",
+    "url": "./static/js/2.756f524e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.756f524e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38ee5329b25f1607b7d0",
+    "url": "./static/js/main.ee914e0a.chunk.js"
+  },
+  {
+    "revision": "1d1368f7d445c458fc2a",
+    "url": "./static/js/runtime-main.5e149b2e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ precache-manifest.deb45ab83e4b4a9a130ba2b9f1eea7b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cb7c5b4492e0d0fee994ab396960b94",
+    "url": "./index.html"
+  },
+  {
+    "revision": "59826184f5532ca4ab3a",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ec15ef7eef509181eba4",
+    "url": "./static/js/2.799510d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.799510d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59826184f5532ca4ab3a",
+    "url": "./static/js/main.3bee1586.chunk.js"
+  },
+  {
+    "revision": "1d1368f7d445c458fc2a",
+    "url": "./static/js/runtime-main.5e149b2e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
