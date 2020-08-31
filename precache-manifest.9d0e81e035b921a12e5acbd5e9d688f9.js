93dd1aa2839c71f36bf3a3c199573a5c2e1e50af
Jie Hao Liao <liaojh1998@gmail.com>
2020-08-31 02:26:34 -0500

    Initial design with homepage

---
+++ precache-manifest.9d0e81e035b921a12e5acbd5e9d688f9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f5234d3a165644b2570351ae153fd9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9d441a37100b50c0081",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "7b3a16084da49d87eb33",
+    "url": "/static/css/main.9a094ec9.chunk.css"
+  },
+  {
+    "revision": "e9d441a37100b50c0081",
+    "url": "/static/js/2.66bb0b76.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.66bb0b76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b3a16084da49d87eb33",
+    "url": "/static/js/main.aa7497fa.chunk.js"
+  },
+  {
+    "revision": "16d50208a04408d1bf24",
+    "url": "/static/js/runtime-main.23041e79.js"
+  },
+  {
+    "revision": "a5cacd616864dbf044c260dd686817b6",
+    "url": "/static/media/cv.a5cacd61.pdf"
+  },
+  {
+    "revision": "d41721cdca51b4aa0ba29cae00a0ddc3",
+    "url": "/static/media/profile.d41721cd.png"
+  }
+]);
